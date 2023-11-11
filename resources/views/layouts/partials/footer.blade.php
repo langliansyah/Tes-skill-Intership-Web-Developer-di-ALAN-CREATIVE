4bceaa9578a6fb9b01d253dcfc2c70065344b9ef
gilangaliansyah <gilangaliansyah35@gmail.com>
2023-11-12 00:32:23 +0700

    first commit

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      Alan Creative
+    </div>
+    <strong>&copy; <?php echo date('Y')?> - Point of Sale System Tugas untuk memenuhi tes skill intership</strong> - PHP Laravel
+  </footer>
